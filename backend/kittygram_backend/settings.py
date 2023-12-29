0b19b7e3d415af7d09c4bc86da0114bd8a809dcc
Artem <bogdartem1997@yandex.ru>
2023-12-29 18:14:00 +0000

    Final round

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = os.getenv('DEBUG')
 
@@ -120,3 +120,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
